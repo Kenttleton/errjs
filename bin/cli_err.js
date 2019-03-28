08ec1a56d0efff88f79e11ae862d200a84b2768c
Kent Utterback <kent.utterback@powerdistributors.com>
2019-03-28 17:02:48 -0400

    cli updates

--- bin/cli_err.js
+++ bin/cli_err.js
@@ -4,37 +4,89 @@
  * Module dependencies.
  */
 
+const NODE_ENV = 'development';
 const cli = require('commander');
-const fs = require(fs);
-const ver = process.env.ERRJS_VERSION || '1.0.0';
-const cliErr = cliErr || {};
+const fs = require('fs');
+const watch = require('node-watch');
 
+const cliErr = {};
 cliErr.update = (function(cliErr) {
-  const start = () => {
+  const start = (fileOrDir) => {
     return true;
   };
-  return {start};
+  const file = (f) => {
+    return true;
+  };
+  const directory = (dir) => {
+    return true;
+  };
+  return {start, file, directory};
 })(cliErr, undefined);
 
 cliErr.watch = (function(cliErr) {
-  const start = () => {
-    return true;
+  const start = (file) => {
+    const log = console.log.bind(console);
+    if (!file) file = '../';
+
+    const watcher = watch(file, {
+      recursive: true,
+      filter: /\.js$/,
+    });
+
+    watcher
+        .on('ready', () => {
+          log(`[${NODE_ENV}] Watching Files for Changes...`);
+        })
+        .on('change', (event, filename) => {
+          const filepath = filename.replace(/\\/g, '/');
+          cliErr.update.file(filepath);
+          log(`${event} occurred on ${filename}`);
+        })
+        .on('error', (error) => {
+          log(error);
+        });
   };
   return {start};
 })(cliErr, undefined);
 
 cliErr.config = (function(cliErr) {
+  const base = require('../bin/cliErr.conf.json');
   const start = (file) => {
-    if (file) return true;
-    return false;
+    updateConfig(file);
   };
-  return {start};
+  const check = () => {
+    return base.override;
+  };
+  const reset = () => {
+    cliErr.JSON.package.version = packageJSON.version;
+  };
+  const updateConfig = (file) => {
+    if (file) {
+      fs.copyFile(file, __dirname + 'override-conf.json', (err) => {
+        if (err) throw err;
+        updatePackage(true);
+
+        console.log('Configuration Updated!');
+      });
+      updatePackage(false);
+      return true;
+    }
+  };
+  const updatePackage = (override) => {
+    if (override) {
+      useOverride = true;
+    }
+    const packageJSON = require('../package.json');
+    base.package.version = packageJSON.version;
+  };
+  return {start, reset, base, check};
 })(cliErr, undefined);
 
 cli
-    .version(ver, '-v, --version')
+    .version(cliErr.config.base.package.version, '-v, --version')
     .option('-u, --update', 'Update filenames and line numbers')
     .option('-w, --watch', 'Watch for updates and edit on save')
+    .option('-r, --reset', 'Reset config to default')
     .option(
         '-c, --config [file]',
         'Provide a config file [./path/to/file.json]',
@@ -42,6 +94,8 @@ cli
     )
     .parse(process.argv);
 
+if (cli.version) cliErr.config.start();
 if (cli.update) cliErr.update.start();
 if (cli.watch) cliErr.watch.start();
 if (cli.config) cliErr.config.start(cli.file);
+if (cli.reset) cliErr.config.reset();
